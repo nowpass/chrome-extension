04748a7b6f1f824156be4eb156a80baec8fbd94e
Yves Hoppe <yves.hoppe@community.joomla.org>
2018-01-14 14:23:19 +0100

    Added simple gulp build script

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,18 @@
-var gulp = require('gulp');
+const gulp = require('gulp');
+const zip = require('gulp-zip');
+const fs = require('fs');
+const config = require('./build.config.json');
 
+const destination = './dist/' + config.version;
+
+gulp.task('default', function () {
+    try {
+        fs.mkdirSync(destination);
+    } catch (err) {
+        // Ignore
+    }
+
+    return gulp.src('extension/**/*')
+        .pipe(zip('nowpass-' + config.version + '.zip'))
+        .pipe(gulp.dest(destination));
+});
