4d0391d01b552bc879c266b7a21bdb59721e8cb4
Yves Hoppe <yves.hoppe@community.joomla.org>
2018-01-16 09:40:55 +0100

    Added translations and updated build script

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 const gulp = require('gulp');
 const zip = require('gulp-zip');
-const fs = require('fs');
+const fs = require('fs-extra');
 const config = require('./build.config.json');
 
 const destination = './dist/' + config.version;
@@ -8,22 +8,34 @@ const fileName = 'nowpass-' + config.version + '.zip';
 
 // Simple deployment and packaging script
 gulp.task('default', function () {
+    console.log('Use build')
+});
+
+gulp.task('clean', function () {
+    try {
+        fs.removeSync(destination);
+    } catch (err) {
+        // Ignore
+    }
+
     try {
         fs.mkdirSync(destination);
     } catch (err) {
         // Ignore
     }
 
-    try {
-        fs.unlinkSync(destination + '/' + fileName);
-    } catch (err) {
-        // Ignore
-    }
+    return true;
+});
 
-    gulp.src('extension/**/*')
+gulp.task('copy', ['clean'], function () {
+    return gulp.src('extension/**/*')
         .pipe(gulp.dest(destination));
+});
 
-    gulp.src(destination + '/**')
+gulp.task('zip', ['copy'], function () {
+    return gulp.src(destination + '/**')
         .pipe(zip(fileName))
         .pipe(gulp.dest(destination));
 });
+
+gulp.task('build', ['clean', 'copy', 'zip']);
